a1379bbc24212526ac80cd7d63e1ee2ec8e2cb30
robel <rob@gem>
2022-12-15 12:49:14 -0800

    Task 2 multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiply to int
+ * @a: first int
+ * @b: second int
+ * Return: Addition of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
