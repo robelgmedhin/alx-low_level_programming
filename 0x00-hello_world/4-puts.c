a360ea01040f8eca56c501037584e70c52521472
robel <rob@gem>
2022-12-16 16:24:48 -0800

    Task 4 puts reviewed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
- * Return: Always 0 (Success)
+ * main - print a line of code using puts
+ *
+ * Return: 0
  */
-
 int main(void)
 {
-	puts("\" Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
